4c312d5a25d8e6c77c0ed6ae29f765e443d85227
DoctHoliday <doct.holiday15@gmail.com>
2022-02-22 18:25:21 -0500

    Testing Commits

---
+++ Test App/Test App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Commits through Visual Studio");
+        }
+    }
+}
