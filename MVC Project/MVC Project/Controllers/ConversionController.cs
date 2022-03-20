0d4645e134e237916e430c15623abb7416ac02ac
DoctHoliday <doct.holiday15@gmail.com>
2022-03-20 19:30:53 -0400

    eh

---
+++ MVC Project/MVC Project/Controllers/ConversionController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+using ImageGear.Core;
+using ImageGear.Formats;
+using ImageGear.Formats.PDF;
+
+namespace MVC_Project.Controllers
+{
+    public class ConversionController : Controller
+    {
+
+        class ConvertDocumentOptions
+        {
+            private String filePath = String.Empty;
+            public String InputPath
+            {
+                get { return filePath; }
+                set { filePath = value; }
+            }
+            private String outputDirectory = String.Empty;
+            public String OuputDir
+            {
+                get { return outputDirectory; }
+                set { outputDirectory = value; }
+            }
+        }
+
+
+        // GET: Conversion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void ProcessDocuments()
+        {
+            Queue<ConvertDocumentOptions> workQueue = new Queue<ConvertDocumentOptions>();
+
+            string inputDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../Samples"));
+            string outputDirectory = Path.Combine(inputDirectory, "Outputs");
+
+            workQueue = CreateDocumentOptions(inputDirectory, outputDirectory);
+
+        }
+
+
+    }
+}
---
+++ MVC Project/MVC Project/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using ImageGear.Core;
+using ImageGear.Evaluation;
+using ImageGear.Formats;
+using ImageGear.Formats.PDF;
+
+namespace MVC_Project
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            ImGearEvaluationManager.Initialize();
+
+            ImGearCommonFormats.Initialize();
+
+            ImGearFileFormats.Filters.Insert(0, ImGearPDF.CreatePDFFormat(@"C:\Users\Kite\Documents\GitHub\Csharp-Stuff\MVC Project\MVC Project\bin\"));
+
+            ImGearPDF.Initialize();
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            ImGearPDF.Terminate();
+        }
+    }
+}
